86ef71ba66db2c70c94cd326f01bbba84e691fee
Ranuda Perera <ranudaperera@gmail.com>
2026-09-23 12:26:15 +0530

    configure: BCrypt password encoder

--- account-service/src/main/java/com/ridelink/account_service/service/AccountService.java
+++ account-service/src/main/java/com/ridelink/account_service/service/AccountService.java
@@ -1,14 +1,20 @@
 package com.ridelink.account_service.service;
 
 import com.ridelink.account_service.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AccountService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AccountService(UserRepository userRepository) {
+    public AccountService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder
+    ) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 }
